0b2b77a409b82c2488d63dd272ee900a834a36a8
Hermes <hermesbarbosa9@gmail.com>
2025-09-21 11:35:19 -0300

    teste 2

--- portifolio/vite.config.js
+++ portifolio/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/hermessoftwareengineer.io/',
   plugins: [
     react(),
     tailwindcss(),
